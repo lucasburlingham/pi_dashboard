648691b1f075480018177f52572eae166b7755e4
Lucas Burlingham <lucas@lucasburlingham.me>
2023-12-10 01:34:42 -0500

    Attempt to fix pages not displaying

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
-$debug = (bool) TRUE;
+// $debug = (bool) TRUE;
 
 require_once $_SERVER['DOCUMENT_ROOT']."/assets/loader.php";
 
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
+
 	<?php
 		// Load css and js
 		loadCSS();
 		loadJS();
 	?>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>pi_dashboard</title>
@@ -21,31 +24,31 @@ require_once $_SERVER['DOCUMENT_ROOT']."/assets/loader.php";
 <body lang="en_US">
 	<div class="container-fluid">
 		<!-- TOP ROW -->
-		<div class="row">
+		<div class="row" height="50vh">
 			<!-- TOP ROW - LEFT SIDE -->
-			<div class="col">
+			<div class="col" width="50vw">
 				<div class="bold mt-1 mx-auto align-top" id="title">Current Weather in Fort Bragg, NC:</div>
 				<div class="my-auto" id="content">
 					<?php include $_SERVER['DOCUMENT_ROOT']."/pages/top-left.php" ?>
 				</div>
 			</div>
 			<!-- TOP ROW - RIGHT SIDE -->
-			<div class="col">
+			<div class="col" width="50vw">
 				<?php include $_SERVER['DOCUMENT_ROOT']."/pages/top-right.php" ?>
 			</div>
 		</div>
 		<!-- BOTTOM ROW -->
-		<div class="row">
+		<div class="row" height="50vh">
 			<!-- BOTTOM ROW - LEFT SIDE -->
-			<div class="col">
+			<div class="col" width="50vw">
 				<div class="bold mt-1 mx-auto align-top" id="title"></div>
 				<div id="content" class="font-italic">
 					<?php include $_SERVER['DOCUMENT_ROOT']."/pages/bottom-left.php" ?>
 				</div>
 			</div>
 			<!-- BOTTOM ROW - RIGHT SIDE -->
-			<div class="col">
-			<?php include $_SERVER['DOCUMENT_ROOT']."/pages/bottom-right.php" ?>
+			<div class="col" width="50vw">
+				<?php include $_SERVER['DOCUMENT_ROOT']."/pages/bottom-right.php" ?>
 			</div>
 		</div>
 	</div>
--- pages/bottom-right.php
+++ pages/bottom-right.php
@@ -1,1 +1,37 @@
 <?php
+
+// get the song name from the file at /tmp/song.txt
+if (file_exists("song.txt")) {
+
+	// convert the file to an array of lines
+	$lines = file("song.txt");
+
+	foreach($lines as $key){
+		echo '<!--'.$key.'-->';
+
+		if (str_starts_with($key, "mpris:artUrl") === TRUE) {
+			// remove the "mpris:artUrl: " from the string
+			$key = str_replace("mpris:artUrl;", "", $key);
+			// remove the newline character from the string
+			$key = str_replace("\n", "", $key);
+			// assign the string to the $art_url variable
+			$art_url = $key;
+		} else if (str_starts_with("xesam:title", $key) === TRUE) {
+			$key = str_replace("xesam:title;", "", $key);
+			$song_title = $key;
+		} else if (str_starts_with("xesam:artist", $key) === TRUE) {
+			$key = str_replace("xesam:artist;", "", $key);
+			$song_artist = $key;
+		} else if (str_starts_with("mpris:length;", $key) === TRUE) {
+			$key = str_replace("mpris:length;", "", $key);
+			$song_album = $key;
+		}
+	}
+} else {
+	echo "File does not exist";
+}
+?>
+
+<div class="text-center" id="content" style='background-image: url("<?php echo $art_url; ?>");'>
+	<!-- <img src="" class="rounded mx-auto d-block img-fluid"> -->
+</div>
